1ec3f302724297366bc0df2a261c78d8d2d47a0e
JagpreetGrewal <jaggy111@gmail.com>
2023-07-26 16:02:44 -0700

    changed some links and the about me section

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,19 +3,18 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Jagpreet Grewal',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Jagpreet Grewal | SFU Engineering Co-op Student`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
-    // Optional: Twitter account handle
-    author: `@rfitzio`,
+    description: `Seeking an internship for September 2023.`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/JagpreetGrewal`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/jagpreet-g-587977105/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `I am a SFU Engineering Co-op Student, in my final year of undergrad. In the future, I plan on completing a Master's degree in Business or in a Big Data field - right now, I am eager to get into the workplace. I have previous co-op experience with Microserve, where I worked as a Deployment Technician for eight months. 
+            Hobbies: I like reading and travelling. I am a part of SFU's book club and I plan on travelling around North America once I get my feet under me.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
